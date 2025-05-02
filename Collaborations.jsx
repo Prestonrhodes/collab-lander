1a480fd8b10b23d98c6c64c4a15deb2615a7fef4
prestonrhodes <preston@instantscalability.com>
2025-05-02 18:04:26 -0400

    Initial commit

---
+++ Collaborations.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Collaborations = () => {
+  return (
+    <div>
+      {/* Your component content here */}
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Collaborations />
+    </div>
+  );
+}
+
+export default App; 
